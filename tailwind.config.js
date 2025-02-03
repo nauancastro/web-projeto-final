085af8f3884d0ac8f202eb4f2013f1000b894264
fernandocastro <fernandopaulino00@gmail.com>
2025-02-03 12:09:34 -0300

    :construction: Otimizando config para ignorar api, componentizando, resolvendo links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js,ts}",  // Garante que Tailwind analise APENAS o front-end
+    "./index.html"  // Inclui o HTML principal na raiz do projeto
+  ],
   theme: {
     extend: {
       backgroundImage: {
